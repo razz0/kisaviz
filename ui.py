b5884488fd6277f3f15041e8497ea2914706ad74
Mikko Koho <mikko.koho@iki.fi>
2017-02-22 20:15:44 +0200

    Refactor ui.py a bit

--- ui.py
+++ ui.py
@@ -8,13 +8,64 @@ Handle UI
 # TODO: Refactor
 
 import numpy as np
+from bokeh.io import output_file, save
 from bokeh.layouts import widgetbox
 from bokeh.models import HBox, CustomJS, ColumnDataSource, WidgetBox
 from bokeh.models.widgets import Panel, Tabs, Select, DataTable, TableColumn
-from bokeh.io import output_file, show, save
 from bokeh.plotting import figure
 
 from data import PinnakisaData
+
+
+def daily_ticks_by_species(data_source):
+    """
+    A line chart visualization of daily ticks by species.
+
+    :param data_source: ColumnDataSource
+    :return: Panel
+    """
+    sp_callback = CustomJS(args=dict(source=data_source), code="""
+            var data = source.data;
+            var f = cb_obj.value;
+
+            data['ticks'] = data[f];
+            source.trigger('change');
+        """)
+
+    fig = figure(plot_width=PLOT_WIDTH, plot_height=PLOT_HEIGHT, x_axis_type='datetime',
+                 title="Pinnoja lajeittain per päivä")
+    fig.line('dates', 'ticks', source=data_source, line_width=3, color="navy", alpha=0.5)
+
+    select = Select(title="Option:", options=['-- Yhteensä --'] + all_species, callback=sp_callback)
+
+    layout = HBox(widgetbox(select), fig)
+    return Panel(child=layout, title="Lajit päivittäin")
+
+
+def daily_most_common_ticks():
+    """
+    A Table view of the most common ticks per day.
+
+    :return: Panel
+    """
+    ticks = kisa.get_daily_popular_ticks(start_date, end_date)
+    tick_source = {}
+    # print(ticks)  # TODO: Handle empty species names somewhere
+    dates, species, count = zip(*((dat, sp[0], sp[1]) for (dat, sp) in ticks))
+
+    tick_source['date'] = dates
+    tick_source['species'] = species
+    tick_source['count'] = count
+
+    tick_source = ColumnDataSource(tick_source)
+
+    columns = [TableColumn(field="date", title="Päivämäärä"),
+               TableColumn(field="species", title="Päivän yleisin laji"),
+               TableColumn(field="count", title="Pinnojen lukumäärä")]
+    table = DataTable(source=tick_source, columns=columns, width=PLOT_WIDTH, height=PLOT_HEIGHT)
+    layout = WidgetBox(table)
+
+    return Panel(child=layout, title="Päivien yleisimmät")
 
 
 PLOT_WIDTH = 1100
@@ -46,28 +97,9 @@ species_data['dates'] = initial_x
 species_data['ticks'] = totals
 species_data['-- Yhteensä --'] = totals
 
+tab1 = daily_ticks_by_species(ColumnDataSource(species_data))
 
-output_file("index.html")
-
-#############
-
-sp_source = ColumnDataSource(species_data)
-
-sp_callback = CustomJS(args=dict(source=sp_source), code="""
-        var data = source.data;
-        var f = cb_obj.value;
-
-        data['ticks'] = data[f];
-        source.trigger('change');
-    """)
-
-p1 = figure(plot_width=PLOT_WIDTH, plot_height=PLOT_HEIGHT, x_axis_type='datetime', title="Pinnoja lajeittain per päivä")
-p1.line('dates', 'ticks', source=sp_source, line_width=3,  color="navy", alpha=0.5)
-
-select = Select(title="Option:", options=['-- Yhteensä --'] + all_species, callback=sp_callback)
-
-layout = HBox(widgetbox(select), p1)
-tab1 = Panel(child=layout, title="Lajit päivittäin")
+tab2 = daily_most_common_ticks()
 
 #############
 
@@ -87,28 +119,7 @@ tab1 = Panel(child=layout, title="Lajit päivittäin")
 
 #############
 
-ticks = kisa.get_daily_popular_ticks(start_date, end_date)
-tick_source = {}
-# print(ticks)  # TODO: Handle empty species names somewhere
-dates, species, count = zip(*((dat, sp[0], sp[1]) for (dat, sp) in ticks))
+tabs = Tabs(tabs=[tab1, tab2])
 
-tick_source['date'] = dates
-tick_source['species'] = species
-tick_source['count'] = count
-
-tick_source = ColumnDataSource(tick_source)
-
-columns = [TableColumn(field="date", title="Päivämäärä"),
-           TableColumn(field="species", title="Päivän yleisin laji"),
-           TableColumn(field="count", title="Pinnojen lukumäärä")]
-table = DataTable(source=tick_source, columns=columns, width=PLOT_WIDTH, height=PLOT_HEIGHT)
-layout = WidgetBox(table)
-
-tab4 = Panel(child=layout, title="Päivien yleisimmät")
-
-#############
-
-tabs = Tabs(tabs=[tab1,  # tab2, tab3,
-                  tab4])
-
+output_file("index.html")
 save(tabs)
